98b80b4f4193008e419b8e4401360e8a91929f5a
Jeremie Rodriguez <jeremie.rodriguez@kobionic.com>
2020-12-23 20:12:00 +0100

    test(app): set static app version to fix pipeline

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,9 @@
 import { render } from '@testing-library/react';
+import config from 'config';
 import React from 'react';
 import App from './App';
+
+config.version = '0.0.0';
 
 test('renders App without error', () => {
   const tree = render(<App />);
